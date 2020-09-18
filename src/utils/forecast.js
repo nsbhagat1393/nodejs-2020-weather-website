880025ba2686963d29a9dfc3c15c0688659fd4d1
Narayan <nsbhagat1304@gmail.com>
2020-09-18 20:37:44 +0530

    Added high and low temperature text on search

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude,longitude,callback)=>{
           callback("Unable to find location",undefined)
        }
        else{
-          callback(undefined,body.daily.data[0].summary+" It is currently "+ body.currently.temperature+ " degrees out. There is "+ body.currently.precipProbability +" % chance of rain")
+          callback(undefined,body.daily.data[0].summary+" It is currently "+ body.currently.temperature+ " degrees out. There is "+ body.currently.precipProbability +" % chance of rain.The high temperature is "+body.daily.data[0].temperatureHigh+" and low temperature is "+body.daily.data[0].temperatureLow)
        }
     })
  }
